6196b4ae1f2f1cd816839c2cf21af762cc4de827
ReZoR-Q2 <rezorgerman@gmail.com>
2025-04-03 21:24:49 +0300

    home

--- dev_env/app/main/views.py
+++ dev_env/app/main/views.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'main/index.html')
+    context:dict[str, str] = {
+        'title': 'Home',
+        'content': 'Главная страница магазина - Home',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+    
+    return render(request, 'main/index.html', context)
     
 def about(request):
     return HttpResponse('About page')
