8436dc69dec4d3ce28983a3d54a511e43c79c2fe
ReZoR-Q2 <rezorgerman@gmail.com>
2025-04-03 19:55:59 +0300

    html

--- dev_env/app/main/views.py
+++ dev_env/app/main/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Home page')
+    return render(request, 'main/index.html')
     
 def about(request):
     return HttpResponse('About page')
